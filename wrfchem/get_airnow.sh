deb79e735db9580a951bb1a8b3cff72d01ef1bd4
dwfncar <fillmore@ucar.edu>
2024-05-22 08:50:42 -0600

    Updated melodies-monet path.

--- wrfchem/get_airnow.sh
+++ wrfchem/get_airnow.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export PYTHONPATH=/glade/u/home/fillmore/MELODIES-MONET_develop_forecast:$PYTHONPATH
-export PYTHONPATH=/glade/u/home/fillmore/monet:$PYTHONPATH
-export PYTHONPATH=/glade/u/home/fillmore/monetio:$PYTHONPATH
+export PYTHONPATH=/glade/u/home/fillmore/MELODIES-MONET:$PYTHONPATH
+export PYTHONPATH=/glade/u/home/fillmore/MONET:$PYTHONPATH
+export PYTHONPATH=/glade/u/home/fillmore/MONETIO:$PYTHONPATH
 
 yesterday_str=$(date --date=yesterday '+%Y%m%d')
 today_str=$(date '+%Y%m%d')
 echo $today_str
-/glade/u/home/fillmore/miniconda3/envs/melodies-monet/bin/melodies-monet get-airnow --start-date $yesterday_str --end-date $today_str -o /glade/work/fillmore/Data/AirNow/AirNow_$yesterday_str.nc
+/glade/u/home/fillmore/miniconda3/bin/melodies-monet get-airnow --start-date $yesterday_str --end-date $today_str -o /glade/work/fillmore/Data/AirNow/AirNow_$yesterday_str.nc
